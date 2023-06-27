dd2c5bd4b6b636422098d2f64648521908f8e2a3
FedericoNicolasBurgos <fedewf1@gmail.com>
2023-06-27 18:31:02 -0300

    Estructura de directorio para el tpfinal grupo 45 2023

---
+++ TPFinal_2023_Grupo45/src/main/java/ar/edu/unju/fi/TpFinal2023Grupo45Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinal2023Grupo45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinal2023Grupo45Application.class, args);
+	}
+
+}
---
+++ TPFinal_2023_Grupo45/src/test/java/ar/edu/unju/fi/TpFinal2023Grupo45ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinal2023Grupo45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
