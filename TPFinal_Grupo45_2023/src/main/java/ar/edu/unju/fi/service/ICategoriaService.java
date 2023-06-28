ac950d25f68be867e2d2bdd7c79888bff4cf42cf
cintiabelenestrada <cintiabelenestrada@gmail.com>
2023-06-28 00:06:09 -0300

    Estrada_estructura_base_del_proyecto

--- TPFinal_Grupo45_2023/src/main/java/ar/edu/unju/fi/service/ICategoriaService.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unju.fi.service;
-
-import java.util.List;
-
-import ar.edu.unju.fi.entity.Categoria;
-
-public interface ICategoriaService {
-	List<Categoria> getCategoria(String dia);
-}
---
+++ src/test/java/src/main/java/ar/edu/unju/fi/tp_final_grupo_45/TpFinalGrupo45ApplicationTests.java
@@ -0,0 +1,13 @@
+package src.main.java.ar.edu.unju.fi.tp_final_grupo_45;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalGrupo45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
