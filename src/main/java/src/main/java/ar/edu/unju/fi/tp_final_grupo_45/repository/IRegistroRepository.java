c0d4480a86fb99f91f4ec5209027fd143cd8f1eb
FedericoNicolasBurgos <fedewf1@gmail.com>
2023-07-02 21:07:48 -0300

    Issue TpFinal Grupo 45
    Se crea la clase Usuario y se agregan todos sus
    
    atributos, getters, setters, contrucctor parametrizado
    
    Sus validaciones de atributos, la tabla y columnas.
    
    Se implemta sus pediciones. Se creo su controller
    
    Ser creo su interfaz. Se creo su repository.
    
    Se modigico el archvio pom para agregar las dependecia faltantes.
    
    Se creo el archivo codigodeusuario.html para que muestre el codigo de
    usuario.
    
    Se creo su controller para que devuelva el id del usuario.

---
+++ src/main/java/src/main/java/ar/edu/unju/fi/tp_final_grupo_45/repository/IRegistroRepository.java
@@ -0,0 +1,17 @@
+package src.main.java.ar.edu.unju.fi.tp_final_grupo_45.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import src.main.java.ar.edu.unju.fi.tp_final_grupo_45.entity.Usuario;
+
+public interface IRegistroRepository extends CrudRepository<Usuario,Long> {
+	/**
+     * Busca y devuelve una lista de productos por estado.
+     *
+     * @param estado el estado del producto
+     * @return una lista de productos que coinciden con el estado especificado
+     */
+	public List<Usuario> findByEstado (boolean estado);
+}
