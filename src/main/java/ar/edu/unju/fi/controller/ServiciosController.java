0e57745d5c5a4dd5f49c99748690b3ff82e5b750
JohnRM992 <jonatanrm92@gmail.com>
2023-07-08 07:12:04 -0300

    Se hizo pull, se re envian los mismos cambios

---
+++ src/main/java/ar/edu/unju/fi/controller/ServiciosController.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class ServiciosController {
+	
+	 @GetMapping("/inicio")
+	    public String getInicioPage( Model model) {
+	        
+	        return "index";
+	    }
+	
+	
+	
+	 @GetMapping("/calculoimc")
+	    public String getServiciosPage( Model model) {
+	        
+	        return "calcular_imc";
+	    }
+
+}
