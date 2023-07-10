b4a5228b5c107d3f98fa631086599c435025ab8d
joelrojasaraya <joelrojasaraya653@gmail.com>
2023-07-10 06:01:43 -0300

    Se añade consejos.html
    Se añade ConsejoController
    
    Imagenes consejos1-3
    
    Se realiza pequeños ajustes en ContactoController
    
    Se añaden cometarios faltantes en diversos archivos

--- src/main/java/ar/edu/unju/fi/controller/ConsejoController.java
+++ src/main/java/ar/edu/unju/fi/controller/ConsejoController.java
@@ -5,26 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * Esta es la clase controladora de Contacto.
- * devuelve la pagina de contacto.
+ * Esta es la clase controladora de Consejo.
+ * devuelve la pagina de consejo.
  * @author joel rojas araya Grupo 45
  * @version 1.0.2 date: 9/7/23
  */
 
 @Controller
-public class ContactoController {
+public class ConsejoController {
 //Se llega a una solucion para que el header cambie de forma dinamica los titulos de la pagina seleccionada
 	
-	@GetMapping("/contacto")
+	@GetMapping("/consejos")
 	public String getInicioPage(Model model) {
     // Si en Inicio se selecciona contacto, el header cambiara el titulo por la opcion seleccionada
-    String tituloPagina = "Contacto"; // Establece el valor por defecto que se vera en el header
+    String tituloPagina = "Consejos"; // Establece el valor por defecto que se vera en el header
 
-    // Se realiza el cambio de valor de `tituloPagina` por Contacto
+    // Se realiza el cambio de valor de `tituloPagina` por Consejo
 
     model.addAttribute("tituloPagina", tituloPagina);
-    return "contacto";
+    return "consejos";
 	}
 }
-	
-
